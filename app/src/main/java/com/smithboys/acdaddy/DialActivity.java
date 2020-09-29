87bc9f77b8ccad36dc6f7a68adb99764cee4c2ea
JosephVitko21 <josephvitko21@gmail.com>
2020-09-28 21:55:31 -0400

    added tab layout

--- app/src/main/java/com/smithboys/acdaddy/DialActivity.java
+++ app/src/main/java/com/smithboys/acdaddy/DialActivity.java
@@ -1,14 +1,40 @@
 package com.smithboys.acdaddy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class DialActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager2 viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dial);
+        setContentView(R.layout.swipe_layout);
+        viewPager = findViewById(R.id.view_pager);
+        tabLayout = findViewById(R.id.tabs);
+
+        viewPager.setAdapter(createCardAdapter());
+        new TabLayoutMediator(tabLayout, viewPager,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        tab.setText("Tab " + (position + 1));
+                    }
+                }).attach();
+    }
+
+    private  ViewPagerAdapter createCardAdapter(){
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        return adapter;
     }
 }
