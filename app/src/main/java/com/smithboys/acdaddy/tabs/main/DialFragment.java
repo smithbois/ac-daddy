a6ec35e8fbf968838bcf2eb330e109681c022289
Nick <nicholashulston@gmail.com>
2020-09-30 01:52:00 -0400

    added slider and gets values

--- app/src/main/java/com/smithboys/acdaddy/tabs/main/DialFragment.java
+++ app/src/main/java/com/smithboys/acdaddy/tabs/main/DialFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -12,15 +13,34 @@ import com.smithboys.acdaddy.R;
 
 public class DialFragment extends Fragment {
 
-    public TextView textView;
-    /*
-        This class represents the activity for the dial tab.
-        Use it to load the layout for the tab and run any code.
-         */
+    SeekBar seekBar;
+    TextView textView;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.dial_tab_layout, container, false);
-        textView = v.findViewById(R.id.dial_text);
+
+        seekBar = v.findViewById(R.id.seek_bar);
+        textView = v.findViewById(R.id.text_view);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int degrees = (progress + 60000) / 1000;
+                textView.setText(degrees + "°");
+
+                if (progress < 500 || progress > 19500) {
+                    seekBar.setThumb(getResources().getDrawable(R.drawable.slider_empty_thumb));
+                } else {
+                    seekBar.setThumb(getResources().getDrawable(R.drawable.slider_thumb));
+                }
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
         return v;
     }
 
