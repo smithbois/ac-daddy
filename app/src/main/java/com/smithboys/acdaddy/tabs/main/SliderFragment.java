de728efe4d1d30fc5a686e3b1a9a134cb4ed21d2
Nick <nicholashulston@gmail.com>
2020-10-01 01:03:24 -0400

    updated slider page, added DesiredTempUtil
    replaced red and blue in colors.xml with default_color.

--- app/src/main/java/com/smithboys/acdaddy/tabs/main/SliderFragment.java
+++ app/src/main/java/com/smithboys/acdaddy/tabs/main/SliderFragment.java
@@ -1,17 +1,28 @@
 package com.smithboys.acdaddy.tabs.main;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.fragment.app.Fragment;
 
 import com.smithboys.acdaddy.R;
+import com.smithboys.acdaddy.util.DesiredTempUtil;
 
 public class SliderFragment extends Fragment {
 
@@ -25,6 +36,7 @@ public class SliderFragment extends Fragment {
         seekBar = v.findViewById(R.id.seek_bar);
         textView = v.findViewById(R.id.temp_number);
         final Switch acSwitch = v.findViewById(R.id.toggle_ac);
+
 
         acSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -40,8 +52,8 @@ public class SliderFragment extends Fragment {
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                int degrees = (progress + 60000) / 1000;
-                textView.setText(Integer.toString(degrees));
+                DesiredTempUtil.desiredTemp = (progress + 60000) / 1000;
+                textView.setText(Integer.toString(DesiredTempUtil.desiredTemp));
 
                 if (progress < 500 || progress > 19500) {
                     seekBar.setThumb(getResources().getDrawable(R.drawable.slider_empty_thumb));
@@ -52,6 +64,30 @@ public class SliderFragment extends Fragment {
                 if (acSwitch.isChecked()) {
                     // Send code to update hardware here, using var degrees
                 }
+
+                // Change colors here
+                int c = DesiredTempUtil.getColor();
+                int[][] states = new int[][] {
+                        new int[] {-android.R.attr.state_checked},
+                        new int[] {android.R.attr.state_checked},
+                };
+
+                int[] thumbColors = new int[] {
+                        getResources().getColor(R.color.gray),
+                        c
+                };
+
+                int[] trackColors = new int[] {
+                        getResources().getColor(R.color.gray),
+                        DesiredTempUtil.getColorFaded()
+                };
+
+                Switch toggleAc = v.findViewById(R.id.toggle_ac);
+                DrawableCompat.setTintList(DrawableCompat.wrap(toggleAc.getThumbDrawable()), new ColorStateList(states, thumbColors));
+                DrawableCompat.setTintList(DrawableCompat.wrap(toggleAc.getTrackDrawable()), new ColorStateList(states, thumbColors));
+                ((TextView)v.findViewById(R.id.current_number)).setTextColor(c);
+                // TODO Drawable d = getResources().getDrawable(R.drawable.slider_track);
+
             }
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {}
